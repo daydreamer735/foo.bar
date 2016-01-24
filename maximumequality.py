7c6a1a8d27748f428c2dc240e0007cb286a9594b
daydreamer735 <summer-ice.7@qq.com>
2016-01-24 16:31:49 +0800

    Update maximumequality.py

--- maximumequality.py
+++ maximumequality.py
@@ -2,8 +2,6 @@ def answer(x):
     car = len(x)
     total = sum(x)
     if total % car == 0:
-        solution = total / car
-        return solution
+        return car
     else:
-        solution = car - 1
-        return solution
+        return car - 1
